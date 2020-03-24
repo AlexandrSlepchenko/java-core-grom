719d1355431b510e666474280c13118a7001857d
AlexandrSlepchenko <alexczr@gmail.com>
2020-03-24 15:15:05 +0200

    Lesson 29 HW

---
+++ src/lesson29/HW/Demo.java
@@ -0,0 +1,9 @@
+package lesson29.HW;
+
+public class Demo {
+    public static void main(String[] args) {
+        HashSetTest hashSetTest = new HashSetTest();
+
+        System.out.println(hashSetTest);
+    }
+}
